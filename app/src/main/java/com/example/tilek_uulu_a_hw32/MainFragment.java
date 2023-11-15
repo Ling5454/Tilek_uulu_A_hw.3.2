402fdadcfadf66cf3141e8715ab24e2ba7fa6d63
TilekovAmanjan <Amanenrirj@gmail.com>
2023-11-16 01:49:39 +0600

    Initial commit

---
+++ app/src/main/java/com/example/tilek_uulu_a_hw32/MainFragment.java
@@ -0,0 +1,56 @@
+package com.example.tilek_uulu_a_hw32;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainFragment extends Fragment {
+    Button btnPlus,btnMinus;
+    TextView tvResult;
+    Integer counter = 0;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter +=1;
+                tvResult.setText(counter.toString());
+            }
+        });
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter -=1;
+                tvResult.setText(counter.toString());
+            }
+        });
+    }
+
+    private void initView() {
+        btnPlus =requireActivity().findViewById(R.id.btn_plus);
+        btnMinus = requireActivity().findViewById(R.id.btn_minus);
+        tvResult = requireActivity().findViewById(R.id.tv_result);
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tilek_uulu_A_hw.3.2"
+include(":app")
+ 
